5d8132cad0e549fe270125eb5612538c675847c8
Matthew Trotman <savageguy@gmail.com>
2023-06-15 20:02:57 +1200

    update .gitignore and [id].vue

--- pages/i/[id].vue
+++ pages/i/[id].vue
@@ -15,6 +15,13 @@ const { data: event, error } = await $client.events.getEventWithInvite.useQuery(
 )
 if (error.value !== null || !event.value)
   throw createError({ statusCode: 404, statusMessage: "Page Not Found" })
+
+//TODO check if nominations are closed
+const now = new Date()
+const predictionsOpen = ref(
+  (event.value.predictions_close_date ?? new Date()) > now
+)
+console.log(predictionsOpen.value)
 </script>
 
 <style scoped></style>
