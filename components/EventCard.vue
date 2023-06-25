560ff57f3b1978d33d004607c16246a4e838f856
Matthew Trotman <savageguy@gmail.com>
2023-06-26 00:11:12 +1200

    styling

--- components/EventCard.vue
+++ components/EventCard.vue
@@ -1,23 +1,24 @@
 <template>
-  <NuxtLink
-    :to="'/event/' + event.id"
-    class="flex h-full flex-row items-baseline rounded-lg border border-gray-200 bg-gray-100 shadow hover:bg-gray-200 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 md:max-w-xl"
-  >
-    <div class="flex flex-col space-y-1 p-4">
-      <h5 class="text-2xl font-bold text-gray-900 dark:text-white">
-        {{ event.name }}
-      </h5>
-      <USkeleton
-        v-if="loading"
-        class="h-4 w-[250px] bg-gray-400 dark:bg-gray-500"
-      />
-      <p class="text-xs font-bold text-gray-700 dark:text-gray-400">
-        {{ date }}
-      </p>
-      <div>
-        <UBadge v-if="predicionsOpen" color="red">
-          Predictions close {{ timeAgo }}
-        </UBadge>
+  <NuxtLink :to="'/event/' + event.id">
+    <div
+      class="flex h-full w-full flex-col items-center rounded-lg border border-gray-200 bg-gray-100 shadow hover:bg-gray-200 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+    >
+      <div class="flex w-full flex-col space-y-1 p-4">
+        <h5 class="text-xl font-bold text-gray-900 dark:text-white">
+          {{ event.name }}
+        </h5>
+        <USkeleton
+          v-if="loading"
+          class="h-4 w-[250px] bg-gray-400 dark:bg-gray-500"
+        />
+        <p class="text-xs font-bold text-gray-700 dark:text-gray-400">
+          {{ date }}
+        </p>
+        <div>
+          <UBadge v-if="predicionsOpen" color="red">
+            Predictions close {{ timeAgo }}
+          </UBadge>
+        </div>
       </div>
     </div>
   </NuxtLink>
@@ -52,4 +53,4 @@ onMounted(() => {
 })
 </script>
 
-<style></style>
+<style scoped></style>
