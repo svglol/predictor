2fd803d26da5f53c52b5904e433de874ad29f5a3
Matthew Trotman <savageguy@gmail.com>
2023-06-28 13:08:38 +1200

    Fix order of entered events

--- server/trpc/routers/users.ts
+++ server/trpc/routers/users.ts
@@ -102,6 +102,9 @@ export const usersRouter = createTRPCRouter({
       },
       include: {
         entries: {
+          orderBy: {
+            created_at: "desc",
+          },
           include: {
             event: true,
           },
