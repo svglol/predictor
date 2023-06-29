1e048b1fd56bbefddf3c6ad206aebafd46305360
Matthew Trotman <savageguy@gmail.com>
2023-06-29 16:57:36 +1200

    update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -81,7 +81,7 @@ export default defineNuxtConfig({
     },
   },
   routeRules: {
-    "/": { prerender: true },
-    "/**": { prerender: true },
+    "/": { swr: true },
+    "/**": { swr: true },
   },
 })
