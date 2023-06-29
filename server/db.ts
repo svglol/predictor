79e2a276788800cf286753c7fcd615a531324f5c
Matthew Trotman <savageguy@gmail.com>
2023-06-29 18:16:43 +1200

    update db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,15 +1,16 @@
 import { PrismaClient } from "@prisma/client"
-import type { PrismaClient as PrismaClientEdge } from "@prisma/client/edge"
+import { PrismaClient as PrismaClientEdge } from "@prisma/client/edge"
 
-let globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
-if (process.env.NODE_ENV !== "development")
-  globalForPrisma = globalThis as unknown as { prisma: PrismaClientEdge }
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
 export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
-  })
+  globalForPrisma.prisma || process.env.NODE_ENV === "development"
+    ? new PrismaClient({
+        log: ["error", "warn"],
+      })
+    : new PrismaClientEdge({
+        log: ["error"],
+      })
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma
