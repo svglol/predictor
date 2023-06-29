a66466b173c73a37f55b5675313e2332ea682d03
Matthew Trotman <savageguy@gmail.com>
2023-06-29 17:05:24 +1200

    update nuxt.config.ts and tailwind.config.cjs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -79,8 +79,8 @@ export default defineNuxtConfig({
   //     Inter: [100, 200, 300, 400, 500, 600, 700, 800, 900],
   //   },
   // },
-  routeRules: {
-    "/": { swr: true },
-    "/**": { swr: true },
-  },
+  // routeRules: {
+  //   "/": { swr: true },
+  //   "/**": { swr: true },
+  // },
 })
