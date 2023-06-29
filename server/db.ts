395c303d6a8fc622eed39ed11d521804d26199ef
Matthew Trotman <savageguy@gmail.com>
2023-06-29 19:51:22 +1200

    update db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,16 +1,19 @@
-import { PrismaClient } from "@prisma/client"
+import type { PrismaClient } from "@prisma/client"
 import { PrismaClient as PrismaClientEdge } from "@prisma/client/edge"
 
 let _prisma: PrismaClientEdge | PrismaClient
 console.info("PRISMA_GENERATE_DATAPROXY", process.env.PRISMA_GENERATE_DATAPROXY)
-if (process.env.PRISMA_GENERATE_DATAPROXY) {
-  _prisma = new PrismaClientEdge({
-    log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
-  })
-} else {
-  _prisma = new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
-  })
-}
+_prisma = new PrismaClientEdge({
+  log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
+})
+// if (process.env.PRISMA_GENERATE_DATAPROXY) {
+//   _prisma = new PrismaClientEdge({
+//     log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
+//   })
+// } else {
+//   _prisma = new PrismaClient({
+//     log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
+//   })
+// }
 
 export const prisma = _prisma
