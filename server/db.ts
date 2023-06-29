4b7cbd55e715af1cfb76087a258e95697b56353d
Matthew Trotman <savageguy@gmail.com>
2023-06-29 19:44:28 +1200

    update db.ts

--- server/db.ts
+++ server/db.ts
@@ -3,7 +3,7 @@ import { PrismaClient as PrismaClientEdge } from "@prisma/client/edge"
 
 let _prisma: PrismaClientEdge | PrismaClient
 
-if (process.env.PRISMA_GENERATE_DATAPROXY === "true") {
+if (process.env.PRISMA_GENERATE_DATAPROXY) {
   _prisma = new PrismaClientEdge({
     log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
   })
