55acf1bdd24de49ae73729e1e2e37cbe04f4c2c3
Matthew Trotman <savageguy@gmail.com>
2023-07-03 20:21:09 +1200

    Revert to non edge

--- server/db.ts
+++ server/db.ts
@@ -1,14 +1,13 @@
 import { PrismaClient } from "@prisma/client"
-import { PrismaClient as PrismaClientEdge } from "@prisma/client/edge"
 
-let _prisma: PrismaClientEdge | PrismaClient
+let _prisma: PrismaClient
 
 if (process.env.NODE_ENV === "development") {
   _prisma = new PrismaClient({
     log: ["error", "warn"],
   })
 } else {
-  _prisma = new PrismaClientEdge({
+  _prisma = new PrismaClient({
     log: ["error"],
   })
 }
