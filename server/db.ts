42a70dda5057679508073a9eacc9bdf3ca7dc82a
Matthew Trotman <savageguy@gmail.com>
2023-07-04 22:02:54 +1200

    Revert "Revert to non edge"
    This reverts commit 55acf1bdd24de49ae73729e1e2e37cbe04f4c2c3.

--- server/db.ts
+++ server/db.ts
@@ -1,13 +1,14 @@
 import { PrismaClient } from "@prisma/client"
+import { PrismaClient as PrismaClientEdge } from "@prisma/client/edge"
 
-let _prisma: PrismaClient
+let _prisma: PrismaClientEdge | PrismaClient
 
 if (process.env.NODE_ENV === "development") {
   _prisma = new PrismaClient({
     log: ["error", "warn"],
   })
 } else {
-  _prisma = new PrismaClient({
+  _prisma = new PrismaClientEdge({
     log: ["error"],
   })
 }
