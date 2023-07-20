b0be5415648b4169833497eda88b7bf87a30d2d5
Matthew Trotman <savageguy@gmail.com>
2023-07-20 20:01:56 +1200

    update [trpc].ts

--- server/api/trpc/[trpc].ts
+++ server/api/trpc/[trpc].ts
@@ -4,7 +4,6 @@ import { createTRPCContext } from "~~/server/trpc/trpc"
 
 export default createNuxtApiHandler({
   router: appRouter,
-  //@ts-expect-error Something to do with authjs error
   createContext: createTRPCContext,
   onError:
     process.env.NODE_ENV === "development"
