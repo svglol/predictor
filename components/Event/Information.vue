84e072b4ffc73865aca2ae67abce3dcdf9dee3b3
Matthew Trotman <savageguy@gmail.com>
2023-09-27 11:21:39 +1300

    feat: :lipstick: adding background/border to match other tabs

--- components/Event/Information.vue
+++ components/Event/Information.vue
@@ -2,7 +2,7 @@
   <div class="py-2">
     <div
       v-if="event?.information"
-      class="prose max-w-full dark:prose-invert focus:outline-none"
+      class="prose max-w-full rounded-lg border border-gray-200 bg-white p-6 shadow dark:prose-invert focus:outline-none dark:border-gray-700 dark:bg-gray-800"
       v-html="event.information"></div>
   </div>
 </template>
