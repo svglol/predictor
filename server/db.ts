06c6b2c0643567bf4ee10b566b6e125448acf61a
Matthew Trotman <savageguy@gmail.com>
2023-10-05 15:57:18 +1300

    feat: :card_file_box: use planetscale database drivers for prisma

--- server/db.ts
+++ server/db.ts
@@ -1,10 +1,18 @@
+import { connect } from '@planetscale/database'
+import { PrismaPlanetScale } from '@prisma/adapter-planetscale'
 import { PrismaClient } from '@prisma/client'
+import { fetch as undiciFetch } from 'undici'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
-
+const connection = connect({
+  url: process.env.DATABASE_URL,
+  fetch: undiciFetch,
+})
+const adapter = new PrismaPlanetScale(connection)
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
+    adapter,
     log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
   })
 
