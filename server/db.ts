e29c72c41b18e7d436c28c0f45b8ec6eddfa5f9b
Matthew Trotman <savageguy@gmail.com>
2023-10-10 11:35:52 +1300

    feat: switch to prisma edge (#230)
    * feat: switch to prisma/edge
    
    * chore: update postinstall script
    
    * fix: remove planetscale adapter
    
    * chore: remove unused packages

--- server/db.ts
+++ server/db.ts
@@ -1,20 +1,12 @@
-import { connect } from '@planetscale/database'
-import { PrismaPlanetScale } from '@prisma/adapter-planetscale'
-import { PrismaClient } from '@prisma/client'
-import { fetch as undiciFetch } from 'undici'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
-const connection = connect({
-  url: process.env.DATABASE_URL,
-  fetch: undiciFetch,
-})
-const adapter = new PrismaPlanetScale(connection)
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    adapter,
     log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
-  })
+  }).$extends(withAccelerate())
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma
