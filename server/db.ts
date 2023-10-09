0caca2cacd36aa2e1f80165308f896e9501bbd74
Matthew Trotman <savageguy@gmail.com>
2023-10-10 11:21:25 +1300

    fix: remove planetscale adapter

--- server/db.ts
+++ server/db.ts
@@ -1,19 +1,10 @@
-import { connect } from '@planetscale/database'
-import { PrismaPlanetScale } from '@prisma/adapter-planetscale'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { fetch as undiciFetch } from 'undici'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
-const connection = connect({
-  url: process.env.DATABASE_URL,
-  fetch: undiciFetch,
-})
-const adapter = new PrismaPlanetScale(connection)
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    adapter,
     log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
   }).$extends(withAccelerate())
 
