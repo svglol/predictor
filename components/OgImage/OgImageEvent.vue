7a198ee4d7ee748c738af503847ae3a6fe56d06b
Matthew Trotman <savageguy@gmail.com>
2024-01-20 11:48:51 +1300

    feat: update ogimage layout

--- components/OgImage/OgImageEvent.vue
+++ components/OgImage/OgImageEvent.vue
@@ -3,25 +3,37 @@
     v-if="title !== 'Memespeak Predictor'"
     class="h-full w-full bg-neutral-900 bg-cover"
     :style="src ? `background-image: url(${cldUrl})` : ''">
-    <div class="mx-auto flex flex-wrap items-center justify-between p-12">
+    <div
+      class="absolute left-[-100%] top-0 flex"
+      :style="{
+        width: '200%',
+        height: '200%',
+        backgroundImage: `radial-gradient(circle, rgba(5,5,5, 0.5) 0%,  rgba(5, 5, 5,0.3)  70%, rgba(5, 5, 5,0) 100%)`,
+      }" />
+    <div class="flex h-full flex-col justify-between p-12">
+      <div>
+        <h1 class="mb-4 text-7xl text-white">
+          {{ title }}
+        </h1>
+        <div
+          v-if="description !== 'Memespeak Predictor'"
+          class="max-w-4xl text-5xl leading-tight text-white/90"
+          v-html="description" />
+      </div>
       <div class="flex flex-row items-center gap-4">
         <Logo />
         <h1 class="text-4xl text-white">Memespeak Predictor</h1>
       </div>
-      <div class="relative z-10 flex h-full flex-col">
-        <div>
-          <h1 class="mb-4 text-7xl text-white">
-            {{ title }}
-          </h1>
-          <div
-            v-if="description !== 'Memespeak Predictor'"
-            class="max-w-4xl text-5xl leading-tight text-white/90"
-            v-html="description" />
-        </div>
-      </div>
     </div>
   </div>
   <div v-else class="h-full w-full bg-neutral-900">
+    <div
+      class="absolute left-[-100%] top-0 flex"
+      :style="{
+        width: '200%',
+        height: '200%',
+        backgroundImage: `radial-gradient(circle, rgba(5,5,5, 0.5) 0%,  rgba(5, 5, 5,0.3)  70%, rgba(5, 5, 5,0) 100%)`,
+      }" />
     <div class="mx-auto my-auto p-12">
       <div class="flex flex-row items-center gap-4">
         <Logo :height="100" :width="100" />
@@ -46,7 +58,7 @@ if (src) {
   const { url } = useCldImageUrl({
     options: {
       src: src as string,
-      opacity: '60',
+      opacity: '100',
       height: '600',
       width: '1200',
       crop: 'fill',
