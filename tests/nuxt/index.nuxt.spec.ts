766715bad1bcd17603ca9fb0e243e8378a7ca17a
Matthew Trotman <savageguy@gmail.com>
2024-01-20 23:55:14 +1300

    test: update test

--- tests/nuxt/index.nuxt.spec.ts
+++ tests/nuxt/index.nuxt.spec.ts
@@ -1,7 +1,12 @@
 import { describe, it, expect } from 'vitest'
-
-describe('something', () => {
+import { mountSuspended } from '@nuxt/test-utils/runtime'
+import app from '~/app.vue'
+describe('app', () => {
   it('works', () => {
     expect(useNuxtApp().globalName).toEqual('nuxt')
+  })
+  it('app can be mounted', async () => {
+    const component = await mountSuspended(app, { route: '/' })
+    expect(component).toBeTruthy()
   })
 })
