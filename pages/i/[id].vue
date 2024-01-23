e59d5bfbc23d219f788ac0b66f6f2b12fe8f5b32
Matthew Trotman <savageguy@gmail.com>
2024-01-23 18:10:16 +1300

    fix: re adding seo data to invite page

--- pages/i/[id].vue
+++ pages/i/[id].vue
@@ -16,6 +16,27 @@ const eventId = computed(() => {
 if (error.value !== null || !event.value || !event.value.visible)
   throw createError({ statusCode: 404, statusMessage: 'Page Not Found' })
 
+useHead({
+  title: event.value.name ?? '',
+})
+
+useSeoMeta({
+  ogTitle: event.value?.name,
+  twitterTitle: event.value?.name,
+  twitterCard: 'summary_large_image',
+  ogDescription: event.value?.description,
+  twitterDescription: event.value?.description,
+})
+
+defineOgImage({
+  component: 'OgImageEvent',
+  props: {
+    title: event.value.name ?? '',
+    description: event.value.description,
+    src: event.value.image,
+  },
+})
+
 if (eventId) {
   navigateTo(`/event/${eventId.value}`)
 }
