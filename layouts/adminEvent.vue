121665a982b26bcd8a0f62a5931dd787748c51cf
Matthew Trotman <savageguy@gmail.com>
2024-01-31 18:04:29 +1300

    refactor: replace events tabs with UHorizontalNavigation

--- layouts/adminEvent.vue
+++ layouts/adminEvent.vue
@@ -2,7 +2,9 @@
   <div>
     <NuxtLayout name="admin">
       <div>
-        <AdminEventTabs :id="id" />
+        <UHorizontalNavigation
+          :links="links"
+          class="mb-2 border-b border-gray-200 dark:border-gray-800" />
         <div>
           <slot />
         </div>
@@ -14,4 +16,13 @@
 <script setup lang="ts">
 const route = useRoute()
 const id = computed(() => route.params.id)
+
+const links = [
+  { label: 'Edit', to: `/admin/event/${id.value}` },
+  { label: 'Info', to: `/admin/event/${id.value}/info` },
+  { label: 'Form', to: `/admin/event/${id.value}/form` },
+  { label: 'Option Sets', to: `/admin/event/${id.value}/optionsets` },
+  { label: 'Entries', to: `/admin/event/${id.value}/entries` },
+  { label: 'Results', to: `/admin/event/${id.value}/results` },
+]
 </script>
