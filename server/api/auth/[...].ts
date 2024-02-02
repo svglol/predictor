c0ece33efc436d33f5a643f8d14bfa4a3c14836a
Matthew Trotman <savageguy@gmail.com>
2024-02-02 23:29:23 +1300

    refactor: remove unused imports

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -4,8 +4,6 @@ import type { AuthConfig } from '@auth/core/types'
 import { NuxtAuthHandler } from '#auth'
 import { db } from '~/server/db'
 import { mySqlDrizzleAdapter } from '~/drizzle/drizzleAdapter'
-import { eq } from 'drizzle-orm'
-import { user as userSchema } from '~/drizzle/schema'
 
 // The #auth virtual import comes from this module. You can use it on the client
 // and server side, however not every export is universal. For example do not
