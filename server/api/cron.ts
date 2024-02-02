503bc99790b0635d1d7021f0bc11ebb5372a4771
Matthew Trotman <savageguy@gmail.com>
2024-02-02 22:56:40 +1300

    refactor: wrap in db transaction

--- server/api/cron.ts
+++ server/api/cron.ts
@@ -4,32 +4,36 @@ import { user } from '~/drizzle/schema'
 import { db } from '~/server/db'
 
 export default defineEventHandler(async () => {
-  const users = await db.query.user.findMany({
-    with: {
-      accounts: { where: (account, { eq }) => eq(account.provider, 'discord') },
-    },
-  })
+  await db.transaction(async tx => {
+    const users = await tx.query.user.findMany({
+      with: {
+        accounts: {
+          where: (account, { eq }) => eq(account.provider, 'discord'),
+        },
+      },
+    })
 
-  for (const u of users) {
-    for (const account of u.accounts) {
-      const profile: DiscordProfile = await $fetch(
-        `https://discord.com/api/users/${account.providerAccountId}`,
-        {
-          headers: {
-            Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
-          },
+    for (const u of users) {
+      for (const account of u.accounts) {
+        const profile: DiscordProfile = await $fetch(
+          `https://discord.com/api/users/${account.providerAccountId}`,
+          {
+            headers: {
+              Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+            },
+          }
+        )
+        let imageUrl = ''
+        if (profile.avatar === null) {
+          const defaultAvatarNumber = parseInt(profile.discriminator) % 5
+          imageUrl = `https://cdn.discordapp.com/embed/avatars/${defaultAvatarNumber}.png`
+        } else {
+          const format = profile.avatar.startsWith('a_') ? 'gif' : 'png'
+          imageUrl = `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.${format}`
         }
-      )
-      let imageUrl = ''
-      if (profile.avatar === null) {
-        const defaultAvatarNumber = parseInt(profile.discriminator) % 5
-        imageUrl = `https://cdn.discordapp.com/embed/avatars/${defaultAvatarNumber}.png`
-      } else {
-        const format = profile.avatar.startsWith('a_') ? 'gif' : 'png'
-        imageUrl = `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.${format}`
+        await tx.update(user).set({ image: imageUrl }).where(eq(user.id, u.id))
       }
-      await db.update(user).set({ image: imageUrl }).where(eq(user.id, u.id))
     }
-  }
+  })
   return 'updated'
 })
