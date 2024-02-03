c5c8bf6a29999237932a154dc27bf35b0b0d64eb
Matthew Trotman <savageguy@gmail.com>
2024-02-03 13:36:30 +1300

    test: :white_check_mark: add e2e tests

---
+++ tests/e2e/app.e2e.spec.ts
@@ -0,0 +1,19 @@
+import { setup, createPage, $fetch } from '@nuxt/test-utils/e2e'
+import { describe, it, expect } from 'vitest'
+await setup({
+  browser: true,
+})
+describe('browser', async () => {
+  it('renders the index page', async () => {
+    const page = await createPage('/')
+    const html = await page.innerHTML('body')
+    expect(html).toContain('Memespeak Predictor')
+  })
+})
+
+describe('server-rendered', async () => {
+  it('renders the index page', async () => {
+    const html = await $fetch('/')
+    expect(html).toContain('Memespeak Predictor')
+  })
+})
