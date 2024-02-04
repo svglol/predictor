4294dcb7bddce36a9a192f264aa34e229fdd7169
Matthew Trotman <savageguy@gmail.com>
2024-02-05 10:46:44 +1300

    feat: only update discord avatars if user is still using discord avatar

--- server/api/cron.ts
+++ server/api/cron.ts
@@ -16,23 +16,28 @@ export default defineEventHandler(async () => {
     for (const u of users) {
       for (const account of u.accounts) {
         // @ts-ignore
-        const profile: DiscordProfile = await $fetch(
-          `https://discord.com/api/users/${account.providerAccountId}`,
-          {
-            headers: {
-              Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
-            },
+        if (u.image?.startsWith('https://cdn.discordapp.com') ?? true) {
+          const profile: DiscordProfile = await $fetch(
+            `https://discord.com/api/users/${account.providerAccountId}`,
+            {
+              headers: {
+                Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+              },
+            }
+          )
+          let imageUrl = ''
+          if (profile.avatar === null) {
+            const defaultAvatarNumber = parseInt(profile.discriminator) % 5
+            imageUrl = `https://cdn.discordapp.com/embed/avatars/${defaultAvatarNumber}.png`
+          } else {
+            const format = profile.avatar.startsWith('a_') ? 'gif' : 'png'
+            imageUrl = `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.${format}`
           }
-        )
-        let imageUrl = ''
-        if (profile.avatar === null) {
-          const defaultAvatarNumber = parseInt(profile.discriminator) % 5
-          imageUrl = `https://cdn.discordapp.com/embed/avatars/${defaultAvatarNumber}.png`
-        } else {
-          const format = profile.avatar.startsWith('a_') ? 'gif' : 'png'
-          imageUrl = `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.${format}`
+          await tx
+            .update(user)
+            .set({ image: imageUrl })
+            .where(eq(user.id, u.id))
         }
-        await tx.update(user).set({ image: imageUrl }).where(eq(user.id, u.id))
       }
     }
   })
