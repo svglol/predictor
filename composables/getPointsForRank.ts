c5c5426dee40e27a472da20275ad6879585c646b
Matthew Trotman <savageguy@gmail.com>
2024-02-05 15:18:16 +1300

    refactor: simplify code

--- composables/getPointsForRank.ts
+++ composables/getPointsForRank.ts
@@ -1,26 +1,4 @@
 export const getPointsForRank = (rank: number) => {
-  switch (rank) {
-    case 1:
-      return 25
-    case 2:
-      return 18
-    case 3:
-      return 15
-    case 4:
-      return 12
-    case 5:
-      return 10
-    case 6:
-      return 8
-    case 7:
-      return 6
-    case 8:
-      return 4
-    case 9:
-      return 2
-    case 10:
-      return 1
-    default:
-      return 0
-  }
+  const pointsForRanks = [25, 18, 15, 12, 10, 8, 6, 4, 2, 1]
+  return pointsForRanks[rank - 1] || 0
 }
