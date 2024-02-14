ce71678e9cbca24a43629575625247d889548ac5
Matthew Trotman <savageguy@gmail.com>
2024-02-14 13:43:00 +1300

    feat: update login page

--- pages/auth/sign-in.vue
+++ pages/auth/sign-in.vue
@@ -1,40 +1,41 @@
 <template>
   <div
     class="mx-auto my-auto flex flex-col items-center justify-center space-y-2">
-    <div
-      class="flex flex-col items-center gap-2 rounded-lg border border-gray-300 p-4 dark:border-gray-700">
-      <div class="flex h-5 flex-row items-center gap-2 py-12">
-        <Logo class="h-10 w-10" />
-        <span class="text-xl font-light text-black dark:text-white">
-          Memespeak Predictor
+    <UCard>
+      <div class="flex flex-col items-center gap-2">
+        <div class="flex flex-row items-center gap-2 py-6">
+          <Logo class="h-10 w-10" />
+          <span class="text-xl font-light text-black dark:text-white">
+            Memespeak Predictor
+          </span>
+        </div>
+        <span class="text-lg text-gray-900 dark:text-white">
+          <UIcon name="material-symbols:lock" />
+          Sign in
         </span>
-      </div>
-      <span class="text-2xl font-semibold text-gray-900 dark:text-white">
-        <UIcon name="material-symbols:lock" />
-        Sign in
-      </span>
-      <UDivider />
-      <span class="text-bold text-xl text-red-500">{{ error }}</span>
-      <UForm
-        :schema="schema"
-        :state="state"
-        class="flex w-full flex-col space-y-4"
-        @submit="signIn('sendgrid', { email: state.email })">
-        <UFormGroup label="Email" name="email">
-          <UInput v-model="state.email" />
-        </UFormGroup>
+        <UDivider />
+        <span class="text-bold text-xl text-red-500">{{ error }}</span>
+        <UForm
+          :schema="schema"
+          :state="state"
+          class="flex w-full flex-col space-y-4"
+          @submit="signIn('sendgrid', { email: state.email })">
+          <UFormGroup label="Email" name="email">
+            <UInput v-model="state.email" />
+          </UFormGroup>
 
-        <UButton color="white" type="submit" class="place-self-center">
-          <UIcon name="material-symbols:alternate-email-rounded" />
-          Continue with Email
+          <UButton color="white" type="submit" class="place-self-center">
+            <UIcon name="material-symbols:alternate-email-rounded" />
+            Continue with Email
+          </UButton>
+        </UForm>
+        <UDivider label="or" />
+        <UButton color="white" @click="signIn('discord')">
+          <UIcon name="fa6-brands:discord" />
+          Continue with Discord
         </UButton>
-      </UForm>
-      <UDivider label="or" />
-      <UButton color="white" @click="signIn('discord')">
-        <UIcon name="fa6-brands:discord" />
-        Continue with Discord
-      </UButton>
-    </div>
+      </div>
+    </UCard>
   </div>
 </template>
 
