bc19287359aef869f08ddb3b4fdb15e79cb95515
Matthew Trotman <savageguy@gmail.com>
2024-02-18 21:10:47 +1300

    fix: text color not always showing

--- composables/getRankClass.ts
+++ composables/getRankClass.ts
@@ -1,11 +1,11 @@
 export const getRankClass = (rank: number) => {
   switch (rank) {
     case 1:
-      return 'font-bold text-yellow-500'
+      return 'font-bold !text-yellow-500'
     case 2:
-      return 'font-bold text-gray-400'
+      return 'font-bold !text-gray-400'
     case 3:
-      return 'font-bold text-amber-600'
+      return 'font-bold !text-amber-600'
     default:
       return ''
   }
