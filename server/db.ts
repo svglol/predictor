536b64ee26401c60e74b57f2a49cedf11830dd9d
Matthew Trotman <savageguy@gmail.com>
2024-02-23 21:50:07 +1300

    chore: update drizzle connection to client

--- server/db.ts
+++ server/db.ts
@@ -1,8 +1,8 @@
 import { drizzle } from 'drizzle-orm/planetscale-serverless'
-import { connect } from '@planetscale/database'
+import { Client } from '@planetscale/database'
 import * as schema from '../drizzle/schema'
 // create the connection
-const connection = connect({
+const client = new Client({
   url: process.env.DATABASE_URL,
 })
-export const db = drizzle(connection, { schema })
+export const db = drizzle(client, { schema })
