43dc0a96a1ebf35df9c85f3ea19a599c62017753
Matthew Trotman <savageguy@gmail.com>
2024-03-02 18:37:34 +1300

    feat: adding cloudflare kv for cache

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -77,9 +77,24 @@ export default defineNuxtConfig({
   },
   sitemap: {
     sources: ['/api/__sitemap__/urls'],
+    cacheMaxAgeSeconds: 3600,
   },
   ogImage: {
     fonts: ['Exo:400', 'Exo:700'],
+    defaults: {
+      cacheMaxAgeSeconds: 60 * 60 * 24 * 7 * 1000, // 7 days
+    },
+  },
+  nitro: {
+    storage: {
+      cache: {
+        driver: 'cloudflare-kv-http',
+        accountId: process.env.CF_ACCOUNT_ID,
+        namespaceId: process.env.CF_NAMESPACE_ID,
+        email: process.env.CF_EMAIL,
+        apiKey: process.env.CF_API_KEY,
+      },
+    },
   },
   pwa: {
     strategies: 'generateSW',
