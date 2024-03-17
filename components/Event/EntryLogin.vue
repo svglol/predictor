5a71aa54d8926f7164c77f42528678fd14815bc9
Matthew Trotman <savageguy@gmail.com>
2024-03-17 23:23:26 +1300

    feat: adjust signin text size

--- components/Event/EntryLogin.vue
+++ components/Event/EntryLogin.vue
@@ -2,7 +2,7 @@
   <div class="flex w-full items-center">
     <div class="flex min-h-0 w-full flex-col items-center justify-center py-8">
       <h1
-        class="text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl dark:text-white">
+        class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
         Sign in
       </h1>
       <p class="mt-6 text-center text-base/7 text-gray-500 dark:text-gray-400">
