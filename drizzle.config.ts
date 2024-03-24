8b53c6c2b9c910405b003a2aa11e60af4d889e32
Matthew Trotman <savageguy@gmail.com>
2024-03-25 10:22:06 +1300

    chore: remove unneeded shit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,5 @@
 import type { Config } from 'drizzle-kit'
 
-if (!process.env.DATABASE_URL)
-  throw new Error('DATABASE_URL is missing')
-
 export default {
   schema: './server/database/schema.ts',
   out: './server/database/migrations',
