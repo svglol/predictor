db5c7bf470171d0971f4acce1f871ab12d39ed81
Matthew Trotman <savageguy@gmail.com>
2024-03-25 16:56:48 +1300

    feat: update login page styling

--- pages/auth/sign-in.vue
+++ pages/auth/sign-in.vue
@@ -1,20 +1,20 @@
 <template>
   <div
-    class="m-auto flex flex-col items-center justify-center space-y-2"
+    class="m-auto flex flex-col items-center justify-center"
   >
     <UCard>
-      <div class="flex flex-col items-center gap-2">
-        <div class="flex flex-row items-center gap-2 py-6">
-          <Logo class="size-10" />
-          <span class="text-xl font-light text-black dark:text-white">
-            Memespeak Predictor
+      <div class="flex flex-col items-center gap-4">
+        <div class="flex flex-col items-center">
+          <div class="flex flex-row items-center gap-2 py-6">
+            <Logo class="size-10" />
+            <span class="text-xl font-light text-black dark:text-white">
+              Memespeak Predictor
+            </span>
+          </div>
+          <span class="text-gray-900 dark:text-white">
+            Sign in to continue
           </span>
         </div>
-        <span class="text-lg text-gray-900 dark:text-white">
-          <UIcon name="material-symbols:lock" />
-          Sign in
-        </span>
-        <UDivider />
         <span class="text-bold text-xl text-red-500">{{ error }}</span>
         <UForm
           :schema="schema"
@@ -22,18 +22,17 @@
           class="flex w-full flex-col space-y-4"
           @submit="signIn('sendgrid', { email: state.email })"
         >
-          <UFormGroup label="Email" name="email">
+          <UFormGroup label="Email Address" name="email">
             <UInput v-model="state.email" />
           </UFormGroup>
 
-          <UButton color="white" type="submit" class="place-self-center">
-            <UIcon name="material-symbols:alternate-email-rounded" />
-            Continue with Email
+          <UButton color="white" type="submit" class="place-self-center" size="lg" block>
+            Continue
           </UButton>
         </UForm>
         <UDivider label="or" />
-        <UButton color="white" @click="signIn('discord')">
-          <UIcon name="fa6-brands:discord" />
+        <UButton color="white" size="lg" block @click="signIn('discord')">
+          <UIcon name="skill-icons:discord" />
           Continue with Discord
         </UButton>
       </div>
