d3bd0b3247e91350a7fb8b91754ac91168d0cf64
Matthew Trotman <savageguy@gmail.com>
2024-04-18 20:26:43 +1200

    fix: file upload not working on cf pages

--- server/api/upload.ts
+++ server/api/upload.ts
@@ -1,33 +1,24 @@
-import { v2 as cloudinary } from 'cloudinary'
 import type { UploadApiResponse } from 'cloudinary'
-import { createReadStream } from 'streamifier'
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-  secure: true,
-})
 
 export default defineEventHandler(async (event) => {
-  const body = (await readMultipartFormData(event)) as {
-    name: string
-    filename: string
-    type: string
-    data: Buffer
-  }[]
-  return new Promise<UploadApiResponse>((resolve, reject) => {
-    const stream = cloudinary.uploader.upload_stream(
-      {
-        folder: process.env.CLOUDINARY_FOLDER,
-      },
-      (error, result) => {
-        if (error)
-          return reject(error)
-        if (result)
-          return resolve(result)
-      },
-    )
-    createReadStream(body[0].data).pipe(stream)
-  })
+  const body = (await readMultipartFormData(event)) as { name: string, filename: string, type: string, data: Buffer }[]
+
+  if (!body || !body.length)
+    return { error: 'No file provided' }
+
+  const fileData = body[0]
+  const formData = new FormData()
+  const blob = new Blob([fileData.data], { type: fileData.type })
+  formData.append('file', blob, fileData.filename)
+  formData.append('upload_preset', process.env.CLOUDINARY_UPLOAD_PRESET as string)
+  formData.append('folder', process.env.CLOUDINARY_FOLDER as string)
+  const response = await fetch(`https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/upload`, {
+    method: 'POST',
+    body: formData,
+  }).then(async res => await res.json())
+
+  if (response.error)
+    return { error: response.error.message }
+
+  return response as UploadApiResponse
 })
