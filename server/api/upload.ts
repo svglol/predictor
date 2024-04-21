a71e1d4250d42146de4a2be561ddf87edd3963ae
Matthew Trotman <savageguy@gmail.com>
2024-04-21 22:10:05 +1200

    fix: ensure upload can only be used by authed users

--- server/api/upload.ts
+++ server/api/upload.ts
@@ -1,10 +1,15 @@
 import type { UploadApiResponse } from 'cloudinary'
+import { getServerSession } from '#auth'
+import { authOptions } from '~/server/api/auth/[...]'
 
 export default defineEventHandler(async (event) => {
+  const session = await getServerSession(event, authOptions)
+  if (!session)
+    return createError({ statusCode: 401, statusMessage: 'Unauthorized' })
   const body = (await readMultipartFormData(event)) as { name: string, filename: string, type: string, data: Buffer }[]
 
   if (!body || !body.length)
-    return { error: 'No file provided' }
+    return createError({ statusCode: 400, statusMessage: 'No file provided' })
 
   const fileData = body[0]
   const formData = new FormData()
@@ -18,7 +23,7 @@ export default defineEventHandler(async (event) => {
   }).then(async res => await res.json())
 
   if (response.error)
-    return { error: response.error.message }
+    return createError({ statusCode: 400, statusMessage: response.error.message })
 
   return response as UploadApiResponse
 })
