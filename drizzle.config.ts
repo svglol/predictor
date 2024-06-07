a5ed731cf7dc5682732ee69f8ff4d33ef71c7c5d
Matthew Trotman <savageguy@gmail.com>
2024-06-07 16:02:25 +1200

    chore: update drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
   dialect: 'sqlite',
+  driver: 'turso',
   schema: './server/database/schema.ts',
   out: './server/database/migrations',
 })
